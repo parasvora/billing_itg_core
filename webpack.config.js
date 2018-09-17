30dd622e5e2ed97f434fc207bdc75974804799c6
Paras Vora <paras.vora@PUPVORA.local>
2018-09-17 16:56:08 +0530

    0.0.4

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const config = {
     entry: './jest.config.js',
+    mode : "development",
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: "index.js"
