ffad953fc62552deb93227d78b92f61be137021a
Paras Vora <paras.vora@PUPVORA.local>
2018-09-25 15:18:25 +0530

    Initial Code Setup

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-/*
 const signIn = require ('./core/pages/jenkins/LoginPage')
 
-module.exports = signIn;*/
+module.exports = signIn;
 
+/*
 module.exports = {
     signIn: require('./core/pages/jenkins/LoginPage')
 };
+*/
